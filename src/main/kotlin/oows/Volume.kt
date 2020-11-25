19adeb6f511647286176ff1f03e76564581fd21b
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2020-11-25 12:10:20 +0100

    Extracted Unit into separate file

--- src/main/kotlin/oows/Volume.kt
+++ src/main/kotlin/oows/Volume.kt
@@ -14,19 +14,5 @@ class Volume(amount: Number, private val unit: Unit) {
 
 }
 
-class Unit private constructor(baseUnitRatio: Number) {
-    internal fun ratio(other: Unit) = other.baseUnitRatio / this.baseUnitRatio
-
-    private val baseUnitRatio = baseUnitRatio.toDouble()
-
-    companion object{
-        val cup = Unit(48)
-        val ounce = Unit(6)
-        val teaspoon = Unit(1)
-        val tablespoon = Unit(3)
-    }
-
-}
-
 
 
--- src/test/kotlin/unit/VolumeTest.kt
+++ src/test/kotlin/unit/VolumeTest.kt
@@ -7,12 +7,10 @@ import oows.Volume
 import oows.Unit
 
 
-class MeasumentTest {
+class VolumeTest {
     @Test fun `Volume tests` () {
         assertEquals(Volume(3, Unit.teaspoon), Volume(1, Unit.tablespoon))
-
         assertEquals(Volume(4, Unit.ounce), Volume(0.5, Unit.cup))
-
 
     }
 }
